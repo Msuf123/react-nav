411b3a904150651e7484cb772f039f7e13038a42
Msuf123 <jack@gmil.com>
2023-06-27 20:01:28 +0530

    complete

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -2,8 +2,13 @@ import { Outlet } from 'react-router-dom'
 import style from '../style/main.module.css'
 import Left from './Left'
 import Top from './Top'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { toogle } from './Stor'
 export default function Root(){
+    const dispatch=useDispatch()
+    if(window.screen.width<344){
+        dispatch(toogle(false))
+    }
     const toggle=useSelector((state)=>(state.button))
     return(
         <div className={style.main}>
